9b2a02198375e2df922db17c70935b2a34860f72
minbaby.zhang <zhangshaomin_1990@126.com>
2016-09-21 19:39:10 +0800

    fix php5.6 对于 ssl 的修改，导致无法正常请求数据

--- shanbei.popclipext/index.php
+++ shanbei.popclipext/index.php
@@ -1,15 +1,26 @@
 #!/usr/bin/env php
 <?php
 
-function __($txt) {
+error_reporting(0);
+
+function __($txt)
+{
     echo $txt;
-    die(); 
+    die();
 }
 
 $API_SEARCH = "https://api.shanbay.com/bdc/search/?word=%s";
 
 $url = sprintf($API_SEARCH, getenv('POPCLIP_TEXT'));
-$ret = file_get_contents($url);
+
+$contextOptions = [
+    "ssl" => [
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ],
+];
+
+$ret = file_get_contents($url, false, stream_context_create($contextOptions));
 
 if (empty($ret)) {
     __("ERROR: network error");
@@ -17,7 +28,7 @@ if (empty($ret)) {
 
 $json = json_decode($ret, true);
 
-if(!is_array($json)) {
+if (!is_array($json)) {
     __("ERROR: decode json");
 }
 
@@ -28,7 +39,3 @@ if ($json['status_code'] == 1) {
 }
 
 __(sprintf("【%s】%s", $json['data']['pronunciation'], str_replace("\n", "; ", $json['data']['definition'])));
-
-
-
-
